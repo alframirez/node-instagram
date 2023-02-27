0fcd4dc197afdcaa7f6cd6d9583a97111bd7eb2d
alframirez <alframirezg@gmail.com>
2023-02-26 20:34:55 -0400

    username

--- src/routes/index.js
+++ src/routes/index.js
@@ -48,7 +48,7 @@ router.get("/logout", () => {});
 router.get("/profile", async (req, res) => {
   try {
     // const media = await instagram.get(`${req.session.user_id}`);
-    const media = await instagram.get("users/self");
+    const media = await instagram.get("users/self", { field: "username" });
     // console.log(media);
     // res.json(instagram);
     res.json(media);
